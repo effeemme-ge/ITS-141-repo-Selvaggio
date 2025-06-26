302e080b8d6c5be710b7fdb3fd24d05f178712b4
FM <musorrafiti.f.2005@outlook.com>
2025-06-26 16:49:50 +0200

    Commit test.

--- LibCalculator/Calculator.cs
+++ LibCalculator/Calculator.cs
@@ -6,5 +6,24 @@
         {
             return a + b;
         }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
     }
 }
--- TestCalculator/Test1.cs
+++ TestCalculator/Test1.cs
@@ -10,7 +10,47 @@ namespace TestCalculator
         {
             var calculator = new Calculator();
             var result = calculator.Add(3, 5);
-            Assert.Equals(7, result);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(3, 0);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(3, -1);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Multiply(3, 0);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(15, 5);
+            Assert.AreEqual(3, result);
         }
     }
 }
