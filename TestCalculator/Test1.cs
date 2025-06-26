6bb6e1a8f9ef9a6a93116e4fb6e02195ac498350
FM <musorrafiti.f.2005@outlook.com>
2025-06-26 16:27:44 +0200

    Commit test

---
+++ TestCalculator/Test1.cs
@@ -0,0 +1,16 @@
+﻿using LibCalculator;
+
+namespace TestCalculator
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(3, 5);
+            Assert.Equals(7, result);
+        }
+    }
+}
